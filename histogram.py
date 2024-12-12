fe847c5d0e94f3b14b58c7ab2694ec510b9feca5
fatonidev <achmadfatino@gmail.com>
2024-12-12 22:31:58 +0700

    Perubahan terbaru

--- histogram.py
+++ histogram.py
@@ -2,21 +2,17 @@ import imageio
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Membaca citra dengan mode 'L' untuk citra grayscale
 image = imageio.imread('citra.jpeg', mode='L')
 
-# Histogram Equalization
 def histogram_equalization(img):
     hist, bins = np.histogram(img.flatten(), bins=256, range=[0, 256])
-    cdf = hist.cumsum()  # Cumulative Distribution Function
-    cdf_normalized = cdf * 255 / cdf[-1]  # Normalisasi
+    cdf = hist.cumsum() 
+    cdf_normalized = cdf * 255 / cdf[-1] 
     img_equalized = np.interp(img.flatten(), bins[:-1], cdf_normalized).reshape(img.shape)
     return img_equalized
 
-# Meningkatkan kontras citra
 image_enhanced = histogram_equalization(image)
 
-# Menampilkan hasil
 plt.figure(figsize=(10, 5))
 plt.subplot(1, 2, 1)
 plt.title('Citra Asli')
